02ee4b5a605a837e39523490b15cc9ddbf61a5e7
Mohammed Ferdaus <202421962+MohammedFerdaus@users.noreply.github.com>
2026-04-22 06:22:17 -0400

    Update config.py

--- Chemistry Simulator/config.py
+++ Chemistry Simulator/config.py
@@ -13,40 +13,40 @@ simulation_height = 70
 scale = 10
 
 # Integration units
-dt = 0.01
+dt = 0.008
 bond_check_interval = 5
 molecule_check_interval = 30
 
 # Thermostat units
-gamma = 0.5
-temperature = 1.2
+gamma = 0.7
+temperature = 1.05
 
 # LJ potential units
-epsilon = 1.0
-lj_cuttoff_factor = 2.5
-force_cap = 500
+epsilon = 2
+lj_cutoff_factor = 2.5
+force_cap = 1000
 
 # Element data
 elements = {
-    'H': {'mass': 1.0, 'max_bonds': 1, 'color': (220, 220, 230), 'sigma': 0.8, 'radius_pixel': 7},
-    'O':  {'mass': 16.0, 'max_bonds': 2, 'color': (220, 60, 60), 'sigma': 1.1, 'radius_pixel': 10},
-    'N':  {'mass': 14.0, 'max_bonds': 3, 'color': (80, 130, 220), 'sigma': 1.2, 'radius_pixel': 10},
-    'He': {'mass': 4.0, 'max_bonds': 0, 'color': (160, 230, 160), 'sigma': 1.0, 'radius_pixel': 8}
+    'H':  {'mass': 1.0,  'max_bonds': 1, 'color': (220, 220, 230), 'sigma': 1.8,  'radius_pixel': 18},
+    'O':  {'mass': 16.0, 'max_bonds': 2, 'color': (220, 60, 60),   'sigma': 2.5,  'radius_pixel': 25},
+    'N':  {'mass': 14.0, 'max_bonds': 3, 'color': (80, 130, 220),  'sigma': 2.5,  'radius_pixel': 25},
+    'He': {'mass': 4.0,  'max_bonds': 0, 'color': (160, 230, 160), 'sigma': 2.0,  'radius_pixel': 20},
 }
 
 # Bonding information
-bonding_form_factor = 1.4   # r_form = factor * sigma_ij
+bonding_form_factor = 1.5  # r_form = factor * sigma_ij
 bonding_break_factor = 2.5  # r_break = factor * r0
-spring_k = 300
-dissipation = 0.6
+spring_k = 25
+dissipation = 0.85
 
 bond_lengths = {
-    frozenset({'H', 'H'}): 0.74,
-    frozenset({'H', 'O'}): 0.96,
-    frozenset({'H', 'N'}): 1.01,
-    frozenset({'O', 'O'}): 1.21,
-    frozenset({'N', 'N'}): 1.10,
-    frozenset({'N', 'O'}): 1.15,
+    frozenset({'H', 'H'}): 3.6,  
+    frozenset({'H', 'O'}): 4.3, 
+    frozenset({'H', 'N'}): 4.3,   
+    frozenset({'O', 'O'}): 5.0,  
+    frozenset({'N', 'N'}): 5.0,
+    frozenset({'N', 'O'}): 5.0,
 }
 
 bond_orders = {            # for double/triple line rendering
@@ -60,12 +60,39 @@ velocity_scale = 0.05    # drag pixel distance * this = initial sim velocity
 
 # Molecules information
 known_molecules = {
+    # H and O
     frozenset({('H', 2)}): 'H₂',
     frozenset({('O', 2)}): 'O₂',
-    frozenset({('N', 2)}): 'N₂',
     frozenset({('H', 2), ('O', 1)}): 'H₂O',
-    frozenset({('H', 3), ('N', 1)}): 'NH₃',
-    frozenset({('H', 1), ('O', 1)}): 'OH·',
     frozenset({('H', 2), ('O', 2)}): 'H₂O₂',
+    frozenset({('H', 1), ('O', 1)}): 'OH·',
+    frozenset({('H', 1), ('O', 2)}): 'HO₂·',
+
+    # H and N
+    frozenset({('N', 2)}): 'N₂',
+    frozenset({('H', 3), ('N', 1)}): 'NH₃',
+    frozenset({('H', 4), ('N', 2)}): 'N₂H₄',
+    frozenset({('H', 2), ('N', 1)}): 'NH₂·',
+
+    # N and O
     frozenset({('N', 1), ('O', 1)}): 'NO·',
+    frozenset({('N', 1), ('O', 2)}): 'NO₂·',
+    frozenset({('N', 2), ('O', 1)}): 'N₂O',
+    frozenset({('N', 2), ('O', 3)}): 'N₂O₃',
+
+    # H, N and O
+    frozenset({('H', 1), ('N', 1), ('O', 3)}): 'HNO₃',
+    frozenset({('H', 1), ('N', 1), ('O', 2)}): 'HNO₂',
+    frozenset({('H', 3), ('N', 1), ('O', 1)}): 'NH₂OH',
+    frozenset({('H', 1), ('N', 1), ('O', 1)}): 'HNO',
 }
+
+# Bond angles
+bond_angles = {
+    'O': 104.5,
+    'N': 107,
+    'H': 180
+}
+
+# Angle spring
+angle_spring_k = 80
